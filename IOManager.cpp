4efbd53bc9cb3e9c3430ccc007a6154edef76d71
Cody Brocious <cody.brocious@gmail.com>
2016-05-11 01:54:56 -0600

    Continued working on IO structures.

--- IOManager.cpp
+++ IOManager.cpp
@@ -4,3 +4,9 @@ uint32_t IOManager::open(string fn) {
 	cout << "Attempting to open " << fn << endl;
 	return 0;
 }
+
+uint32_t IOManager::add_handle(shared_ptr<IOHandle> handle) {
+	assert(handle_id < 0xFFFFFFFF);
+	handles[++handle_id] = handle;
+	return handle_id;
+}
--- IOManager.hpp
+++ IOManager.hpp
@@ -10,9 +10,26 @@ public:
 
 class FileHandle : IOHandle {
 public:
+	string filename;
+};
+
+class FSTree : map<string, unique_ptr<FSTree>> {
+};
+
+class DirectoryTree : FSTree {
+public:
+
+};
+
+class FileEntry : FSTree {
+public:
 };
 
 class IOManager {
 public:
 	uint32_t open(string fn);
+	uint32_t add_handle(shared_ptr<IOHandle> handle);
+
+	map<uint32_t, shared_ptr<IOHandle>> handles;
+	uint32_t handle_id;
 };
