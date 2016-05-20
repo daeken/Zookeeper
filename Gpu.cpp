d53e5cb51b11484b4656b7d856f6134fa251cc73
Cody Brocious <cody.brocious@gmail.com>
2016-05-20 13:11:04 -0600

    Made unknown GPU calls non-fatal.

--- Gpu.cpp
+++ Gpu.cpp
@@ -7,13 +7,11 @@ Gpu::Gpu() {
 }
 
 uint32_t Gpu::readMmio(uint32_t addr) {
-	cout << format("Gpu::readMmio(0x%08x)") % addr << endl;
-
 	switch(addr) {
 		case 0xfd100200:
 			return 3;
 		default:
-			break_in(true);
+			cout << format("Gpu::readMmio(0x%08x)") % addr << endl;
 			return 0;
 	}
 }
@@ -22,26 +20,26 @@ void Gpu::writeMmio(uint32_t addr, uint32_t value) {
 	if(addr > 0xfd700000 && addr <= 0xfd705000)
 		return;
 	cout << format("Gpu::writeMmio(0x%08x, 0x%08x)") % addr % value << endl;
-	break_in(true);
+	break_in();
 }
 
 void Gpu::readPci(uint32_t reg, void *buffer, uint32_t length) {
 	cout << format("Gpu::readPci(0x%08x, %i)") % reg % length << endl;
-	break_in(true);
+	break_in();
 }
 
 void Gpu::writePci(uint32_t reg, void *buffer, uint32_t length) {
 	cout << format("Gpu::writePci(0x%08x, %i)") % reg % length << endl;
-	break_in(true);
+	break_in();
 }
 
 uint32_t Gpu::readPort(uint32_t port, uint32_t size) {
 	cout << format("Gpu::readPort(0x%04x, %i)") % port % size << endl;
-	break_in(true);
+	break_in();
 	return 0;
 }
 
 void Gpu::writePort(uint32_t port, uint32_t size, uint32_t value) {
 	cout << format("Gpu::writePort(0x%04x, %i, 0x%x)") % port % size % value << endl;
-	break_in(true);
+	break_in();
 }
--- NightBeliever/XboxKernel/HW.cpp
+++ NightBeliever/XboxKernel/HW.cpp
@@ -8,7 +8,6 @@ void NTAPI kernel_HalReadWritePCISpace(
 	uint32_t Length, 
 	BOOLEAN WritePCISpace
 ) {
-	log("HalReadWritePCISpace(%i, %i, %i, 0x%08x, %i, %i)", BusNumber, SlotNumber, RegisterNumber, Buffer, Length, WritePCISpace);
 	if(WritePCISpace)
 		pci_write(BusNumber, SlotNumber, RegisterNumber, Buffer, Length);
 	else
